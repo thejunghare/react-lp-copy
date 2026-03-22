da8f889bc9fb12bd1900851a004f25d241917e59
thejunghare <prasadjunghare2@gmail.com>
2026-03-22 14:13:31 +0530

    updated challenge

--- index.jsx
+++ index.jsx
@@ -1,11 +1,16 @@
-import {createRoot} from 'react-dom/client';
+/*
+Challenge: Part 1
+Create a custom "Page" component
+It should return an ordered list with your 3 fav pokemon
+Render the page component
 
-const root = createRoot(document.getElementById('root'));
+Challenge: Part 2
+Add a 'header' element with a 'img' element with a image of pokedesk logo inside
+make sure to set width so it doesn't take up the whole screen, also include some alt text on the image
 
-root.render(
-    <main>
+Add an 'H1' with some text describing the page, place it above the ordered list the wrap the 'h1' and 'ol' inside the
+main element to keep the structure
 
-    </main>
-)
-
-// Render with a function
+Add a footer after the list that says
+@2026 your-name development. All rights reserved.
+*/
