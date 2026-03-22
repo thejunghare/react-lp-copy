b7ff45b844daea3851a24aeee41dfd3a9e822c09
thejunghare <prasadjunghare2@gmail.com>
2026-03-22 20:03:05 +0530

    updated code

---
+++ importExport/dummyData.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 1,
+    item: "keyboard",
+  },
+  {
+    id: 2,
+    item: "mouse",
+  },
+  {
+    id: 3,
+    item: "monitor",
+  },
+  {
+    id: 4,
+    item: "Headphones",
+  },
+];
