e8ab28f53b884ff670879cf75d50a7b4995b18b3
thejunghare <prasadjunghare2@gmail.com>
2026-03-22 13:27:52 +0530

    updated code

--- index.jsx
+++ index.jsx
@@ -2,25 +2,10 @@ import {createRoot} from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
 
-function MyAwesomeNavbar() {
-    return (
-        <nav className={"container"}>
-            some header frontend
-        </nav>
-    )
-}
-
-/*
-challenge:
-create your very first custom react component!
-call it MainCotent, and have it return a simple
-h1 element that says 'React is great!'
-Afterward render it on the line below MyAwesomeNavbar
-*/
-
-
 root.render(
-    <div>
-        <MyAwesomeNavbar/>
-    </div>
+    <main>
+
+    </main>
 )
+
+// Render with a function
