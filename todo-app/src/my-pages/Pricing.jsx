6d860609a71ec2b50a8896a385bba4cb5ed5d58c
thejunghare <prasadjunghare2@gmail.com>
2026-05-13 18:19:38 +0530

    updated code on 13 may

--- todo-app/src/my-pages/Pricing.jsx
+++ todo-app/src/my-pages/Pricing.jsx
@@ -1,7 +1,42 @@
+import data from "../data/pricing.json";
+import { useState, useEffect } from "react";
+
 export const Pricing = () => {
-return (
+  const [plans, setPlans] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // setPlans(data);
+    async function getPricing() {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://api.jsonbin.io/v3/qs/6a041905adc21f119a91f495",
+        );
+        console.log(response);
+        const API_DATA = await response.json();
+        setPlans(API_DATA.record);
+        console.log(plans);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getPricing();
+  }, []);
+
+  if (loading) {
+    return <>Loading....</>;
+  }
+
+  return (
     <>
-    <p>This is Pricing page</p>
+      {plans.map((plan) => (
+        <p>
+          {plan.name} {plan.price}
+        </p>
+      ))}
     </>
-)
-}
+  );
+};
